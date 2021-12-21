4f7b060c03a7944cacb9531fe5a30dc5fa14fff9
Michael Schmitt <schmiddim@gmx.at>
2021-12-21 17:44:40 +0100

    TT testing

--- 05-structs/shapes.go
+++ 05-structs/shapes.go
@@ -14,6 +14,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (rectangle Rectangle) Perimeter() (result float64) {
 	result = (rectangle.Width + rectangle.Height) * 2
 	return
@@ -28,5 +33,11 @@ func (rectangle Rectangle) Area() (result float64) {
 func (circle Circle) Area() (result float64) {
 
 	result = circle.Radius * circle.Radius * math.Pi
+	return
+}
+
+func (triangle Triangle) Area() (result float64) {
+	result = (triangle.Base * triangle.Height) * 0.5
+
 	return
 }
