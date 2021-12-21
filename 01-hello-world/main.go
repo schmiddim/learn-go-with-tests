2f1ea537a228a7cf6e3225dcf98b99e8e3ca22fb
Michael Schmitt <schmiddim@gmx.at>
2021-12-21 14:41:55 +0100

    more languages

--- 01-hello-world/main.go
+++ 01-hello-world/main.go
@@ -3,16 +3,27 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+
+	prefix := englishHelloPrefix
+	if language == "Spanish" {
+		prefix = spanishHelloPrefix
+	}
+
+	if language == "French" {
+		prefix = frenchHelloPrefix
+	}
 
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
