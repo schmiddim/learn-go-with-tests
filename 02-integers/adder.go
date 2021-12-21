38e485fd4dbd663c8be563b81c4f7ed1c13a1946
Michael Schmitt <schmiddim@gmx.at>
2021-12-21 14:57:29 +0100

    integers, go documentation

---
+++ 02-integers/adder.go
@@ -0,0 +1,10 @@
+package integers
+
+// Add takes two integers and returns the sum of them
+func Add(a int, b int) (result int) {
+
+	result = a + b
+
+	return
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Learn Go with tests
+
+Code from the tutorial
+
+https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/hello-world
