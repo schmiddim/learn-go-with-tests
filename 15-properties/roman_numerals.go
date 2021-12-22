a615dfc7bc39c90033425a110ddcec9bca54df06
Michael Schmitt <schmiddim@gmx.at>
2021-12-22 18:07:40 +0100

    15 properties ConvertToArabic

--- 15-properties/roman_numerals.go
+++ 15-properties/roman_numerals.go
@@ -1,41 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
-func ConvertToRoman(number int) (roman string) {
-	romanLiterals := []struct {
-		symbol string
-		value  int
-	}{
-		{"M", 1000},
-		{"CM", 900},
-		{"D", 500},
-		{"D", 400},
-		{"C", 100},
-		{"L", 50},
-		{"XL", 40},
-		{"X", 10},
-		{"IX", 9},
-		{"V", 5},
-		{"IV", 4},
-		{"I", 1},
-	}
-
-	for _, l := range romanLiterals {
-		count := number / l.value
-		number -= count * l.value
-		roman += strings.Repeat(l.symbol, count)
-		//fmt.Println(l)
-	}
-	return
-
+var romanLiterals = []struct {
+	Symbol string
+	Value  int
+}{
+	{"M", 1000},
+	{"CM", 900},
+	{"D", 500},
+	{"D", 400},
+	{"C", 100},
+	{"L", 50},
+	{"XL", 40},
+	{"X", 10},
+	{"IX", 9},
+	{"V", 5},
+	{"IV", 4},
+	{"I", 1},
 }
 
-func main() {
+func ConvertToArabic(roman string) (arabic int) {
+	for _, literal := range romanLiterals {
+		if strings.HasPrefix(roman, literal.Symbol) {
+			arabic += literal.Value
+			arabic += ConvertToArabic(roman[len(literal.Symbol):])
+			roman = ""
+			return
+		}
+	}
+	return
+}
 
-	fmt.Println(ConvertToRoman(1984))
+func ConvertToRoman(arabic int) (roman string) {
+
+	for _, literal := range romanLiterals {
+		count := arabic / literal.Value
+		arabic -= count * literal.Value
+		roman += strings.Repeat(literal.Symbol, count)
+	}
+	return
 
 }
--- 15-properties/roman_numerals_test.go
+++ 15-properties/roman_numerals_test.go
@@ -1,46 +1,52 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+var cases = []struct {
+	Description string
+	Arabic      int
+	Roman       string
+}{
+	{"1 gets converted to I", 1, "I"},
+	{"2 gets converted to II", 2, "II"},
+	{"3 gets converted to III", 3, "III"},
+	{"4 gets converted to IV", 4, "IV"},
+	{"9 gets converted to IX", 9, "IX"},
+	{"10 gets converted to X", 10, "X"},
+	{"14 gets converted to XIV", 14, "XIV"},
+	{"18 gets converted to XVIII", 18, "XVIII"},
+	{"20 gets converted to XX", 20, "XX"},
+	{"39 gets converted to XXXIX", 39, "XXXIX"},
+	{"40 gets converted to XL", 40, "XL"},
+	{"47 gets converted to XLVII", 47, "XLVII"},
+	{"49 gets converted to XLIX", 49, "XLIX"},
+	{"50 gets converted to L", 50, "L"},
+	{"789 gets converted to DCCLXXXIX", 789, "DCCLXXXIX"},
+	{"1954 gets converted to MCMLIV", 1954, "MCMLIV"},
+	{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
+}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+func TestConvertingToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
 	}
 }
 
-func TestArea(t *testing.T) {
-	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
-	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
-		{"9 gets converted to IX", 9, "IX"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"39 gets converted to XXXIX", 39, "XXXIX"},
-		{"40 gets converted to XL", 40, "XL"},
-		{"47 gets converted to XLVII", 47, "XLVII"},
-		{"49 gets converted to XLIX", 49, "XLIX"},
-		{"50 gets converted to L", 50, "L"},
-		{"789 gets converted to DCCLXXXIX", 789, "DCCLXXXIX"},
-		{"1954 gets converted to MCMLIV", 1954, "MCMLIV"},
-		{"1984 gets converted to MCMLXXXIV", 1984, "MCMLXXXIV"},
-	}
+func TestRomanNumerals(t *testing.T) {
 
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
 	}
