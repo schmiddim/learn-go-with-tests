be86244b8e90deca13995789ef2160a6c4951b0f
Michael Schmitt <schmiddim@gmx.at>
2021-12-22 11:46:10 +0100

    Concurrency Test Works

--- 10-concurrency/checkWebsites.go
+++ 10-concurrency/checkWebsites.go
@@ -12,11 +12,13 @@ func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	resultChannel := make(chan result)
 	for _, url := range urls {
 		go func(u string) {
+			// Send statement
 			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
 
 	for i := 0; i < len(urls); i++ {
+		// Receive expressions
 		r := <-resultChannel
 		results[r.string] = r.bool
 	}
