b731f21af097e36ad6b8f938a8b1bfc9f5f8e73a
Michael Schmitt <schmiddim@gmx.at>
2021-12-22 12:57:16 +0100

    Select Test Works

--- 11-select/racer.go
+++ 11-select/racer.go
@@ -7,15 +7,23 @@ import (
 
 func Racer(a, b string) (winner string) {
 
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration > bDuration {
-
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
 		return b
 	}
+}
 
-	return a
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
 }
 
 func measureResponseTime(url string) (t time.Duration) {
