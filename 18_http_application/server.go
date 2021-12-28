486ca5c1f29e0bed700a1cf0b7fa942c792b887c
Michael Schmitt <schmiddim@gmx.at>
2021-12-28 15:03:52 +0100

    18_http_application basic test works

--- 18_http_application/server.go
+++ 18_http_application/server.go
@@ -3,8 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, GetPlayerScore(player))
+
+}
+
+func GetPlayerScore(name string) string {
+	switch name {
+	case "Pepper":
+		return "20"
+
+	case "Floyd":
+		return "10"
+	default:
+		return "the fock"
+
+	}
+
 }
