e745ac096e91764f8899fbc64cc2ed25c192abac
Michael Schmitt <schmiddim@gmx.at>
2021-12-28 11:36:21 +0100

    16-math rougly equal

---
+++ 16-math/clockface.go
@@ -0,0 +1,33 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two dimensional Cartesian coordinate
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+
+}
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+
+}
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
