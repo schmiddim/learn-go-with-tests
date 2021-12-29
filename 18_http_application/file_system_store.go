ff8c7fcb333d375a957a51bebea9eaf59eef4f4d
Michael Schmitt <schmiddim@gmx.at>
2021-12-29 16:09:07 +0100

    io+sorting: sorting

--- 18_http_application/file_system_store.go
+++ 18_http_application/file_system_store.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
 
 //file_system_store.go
@@ -49,9 +50,11 @@ func initialisePlayerDBFile(file *os.File) error {
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
+	sort.Slice(f.league, func(i, j int) bool {
+		return f.league[i].Wins > f.league[j].Wins
+	})
 	return f.league
 }
-
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
 
 	player := f.league.Find(name)
--- 18_http_application/file_system_store_test.go
+++ 18_http_application/file_system_store_test.go
@@ -20,8 +20,8 @@ func TestFileSystemStore(t *testing.T) {
 		got := store.GetLeague()
 
 		want := []Player{
-			{"Cleo", 10},
 			{"Chris", 33},
+			{"Cleo", 10},
 		}
 
 		assertLeague(t, got, want)
@@ -82,6 +82,32 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertNoError(t, err)
 	})
+
+	//file_system_store_test.go
+	t.Run("league sorted", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+        {"Name": "Cleo", "Wins": 10},
+        {"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store, err := NewFileSystemPlayerStore(database)
+
+		assertNoError(t, err)
+
+		got := store.GetLeague()
+
+		want := []Player{
+			{"Chris", 33},
+			{"Cleo", 10},
+		}
+
+		assertLeague(t, got, want)
+
+		// read again
+		got = store.GetLeague()
+		//assertLeague(t, got, want)
+	})
+
 }
 func assertScoreEquals(t *testing.T, got int, want int) {
 	if got != want {
