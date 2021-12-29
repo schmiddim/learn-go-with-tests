3d7e5d9bccbc1508563375a7924bc570ee9a9db6
Michael Schmitt <schmiddim@gmx.at>
2021-12-29 10:57:09 +0100

    io+sorting: FilesystemReader

---
+++ 18_http_application/file_system_store.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.Reader
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+
+	var league []Player
+	json.NewDecoder(f.database).Decode(&league)
+	return league
+}
