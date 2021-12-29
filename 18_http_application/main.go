fa25cbe1049d87756961157f61fdebce0bdb626c
Michael Schmitt <schmiddim@gmx.at>
2021-12-29 11:51:30 +0100

    io+sorting: app uses json db

--- 18_http_application/main.go
+++ 18_http_application/main.go
@@ -1,12 +1,25 @@
+//main.go
 package main
 
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
+const dbFileName = "game.db.json"
 
-	server := NewPlayerServer(NewInMemoryPlayerStore())
-	log.Fatal(http.ListenAndServe(":5000", server))
+func main() {
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := &FileSystemPlayerStore{db}
+	server := NewPlayerServer(store)
+
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
