c7b6248bdc36288a490420f18d83549141315650
Michael Schmitt <schmiddim@gmx.at>
2021-12-30 11:14:13 +0100

    cli+package: main broken bc of unexported fields

--- 18_http_application/cmd/cli/main.go
+++ 18_http_application/cmd/cli/main.go
@@ -1,8 +1,32 @@
 //cmd/cli/main.go
 package main
 
-import "fmt"
+import (
+	poker "github.com/quii/learn-go-with-tests/command-line/v3"
+
+	"fmt"
+	"log"
+	"os"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
 	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v ", err)
+	}
+
+	game := poker.CLI{store, os.Stdin}
+	game.PlayPoker()
 }
